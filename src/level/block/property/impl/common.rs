83dbbc18ed57daf56e8d2b69d9883482d01d1bb8
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-04-17 01:14:01 +1200

    common

--- src/level/block/property/impl/common.rs
+++ src/level/block/property/impl/common.rs
@@ -19,6 +19,34 @@ use crate::level::block::property::r#impl::enums::dirt_type::DirtType;
 use crate::level::block::property::r#impl::enums::double_plant_type::DoublePlantType;
 use crate::level::block::property::r#impl::enums::dripstone_thickness::DripstoneThickness;
 use crate::level::block::property::r#impl::enums::level_direction::LeverDirection;
+use crate::level::block::property::r#impl::enums::minecraft_cardinal_direction::MinecraftCardinalDirection;
+use crate::level::block::property::r#impl::enums::minecraft_vertical_half::MinecraftVerticalHalf;
+use crate::level::block::property::r#impl::enums::monster_egg_stone_type::MonsterEggStoneType;
+use crate::level::block::property::r#impl::enums::new_leaf_type::NewLeafType;
+use crate::level::block::property::r#impl::enums::old_leaf_type::OldLeafType;
+use crate::level::block::property::r#impl::enums::orientation::Orientation;
+use crate::level::block::property::r#impl::enums::pale_moss_carpet_side::PaleMossCarpetSide;
+use crate::level::block::property::r#impl::enums::portal_axis::PortalAxis;
+use crate::level::block::property::r#impl::enums::prismarine_block_type::PrismarineBlockType;
+use crate::level::block::property::r#impl::enums::sand_type::SandType;
+use crate::level::block::property::r#impl::enums::sea_grass_type::SeaGrassType;
+use crate::level::block::property::r#impl::enums::sponge_type::SpongeType;
+use crate::level::block::property::r#impl::enums::stone_brick_type::StoneBrickType;
+use crate::level::block::property::r#impl::enums::stone_slab_type::StoneSlabType;
+use crate::level::block::property::r#impl::enums::stone_slab_type_2::StoneSlabType2;
+use crate::level::block::property::r#impl::enums::stone_slab_type_3::StoneSlabType3;
+use crate::level::block::property::r#impl::enums::stone_slab_type_4::StoneSlabType4;
+use crate::level::block::property::r#impl::enums::structure_block_type::StructureBlockType;
+use crate::level::block::property::r#impl::enums::structure_void_type::StructureVoidType;
+use crate::level::block::property::r#impl::enums::tall_grass_type::TallGrassType;
+use crate::level::block::property::r#impl::enums::torch_facing_direction::TorchFacingDirection;
+use crate::level::block::property::r#impl::enums::turtle_egg_count::TurtleEggCount;
+use crate::level::block::property::r#impl::enums::vault_state::VaultState;
+use crate::level::block::property::r#impl::enums::wall_block_type::WallBlockType;
+use crate::level::block::property::r#impl::enums::wall_connection_type::WallConnectionType;
+use crate::level::block::property::r#impl::enums::wood_type::WoodType;
+use crate::level::block::property::r#type::block_property_type::BlockPropertyType::Int;
+use crate::math::enums::axis::Axis;
 use crate::math::enums::block_face::BlockFace;
 
 pub const ACTIVE: Lazy<BooleanPropertyType> = Lazy::new(|| 
@@ -590,5 +618,554 @@ pub const MINECRAFT_BLOCK_FACE: Lazy<EnumPropertyType> = Lazy::new(||
         "minecraft:block_face",
         BlockFace::VARIANTS,
         BlockFace::VARIANTS[0]
+    )
+);
+
+pub const MINECRAFT_CARDINAL_DIRECTION: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "minecraft:cardinal_direction",
+        MinecraftCardinalDirection::VARIANTS,
+        MinecraftCardinalDirection::VARIANTS[0]
+    )
+);
+
+pub const MINECRAFT_FACING_DIRECTION: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "minecraft:facing_direction",
+        BlockFace::VARIANTS,
+        BlockFace::VARIANTS[0]
+    )
+);
+
+pub const MINECRAFT_VERTICAL_HALF: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "minecraft:vertical_half",
+        MinecraftVerticalHalf::VARIANTS,
+        MinecraftVerticalHalf::VARIANTS[0]
+    )
+);
+
+pub const MOISTURIZED_AMOUNT: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "moisturized_amount",
+        0,
+        7,
+        0
+    )
+);
+
+pub const MONSTER_EGG_STONE_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "monster_egg_stone_type",
+        MonsterEggStoneType::VARIANTS,
+        MonsterEggStoneType::VARIANTS[0]
+    )
+);
+
+pub const MULTI_FACE_DIRECTION_BITS: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "multi_face_direction_bits",
+        0, 
+        63,
+        0
+    )
+);
+
+pub const NEW_LEAF_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "new_leaf_type",
+        NewLeafType::VARIANTS,
+        NewLeafType::VARIANTS[0]
+    )
+);
+
+pub const OCCUPIED_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "occupied_bit",
+        false
+    )
+);
+
+pub const OLD_LEAF_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "old_leaf_type",
+        OldLeafType::VARIANTS,
+        OldLeafType::VARIANTS[0]
+    )
+);
+
+pub const OPEN_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "open_bit",
+        false
+    )
+);
+
+pub const ORIENTATION: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "orientation",
+        Orientation::VARIANTS,
+        Orientation::VARIANTS[0]
+    )
+);
+
+pub const OUTPUT_LIT_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "output_lit_bit",
+        false
+    )
+);
+
+pub const OUTPUT_SUBTRACT_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "output_subtract_bit",
+        false
+    )
+);
+
+pub const PERSISTENT_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "persistent_bit",
+        false
+    )
+);
+
+pub const PILLAR_AXIS: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "pillar_axis",
+        Axis::VARIANTS,
+        Axis::VARIANTS[0]
+    )
+);
+
+pub const PORTAL_AXIS: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "portal_axis",
+        PortalAxis::VARIANTS,
+        PortalAxis::VARIANTS[0]
+    )
+);
+
+pub const POWERED_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "powered_bit",
+        false
+    )
+);
+
+pub const PRISMARINE_BLOCK_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "prismarine_block_type",
+        PrismarineBlockType::VARIANTS,
+        PrismarineBlockType::VARIANTS[0]
+    )
+);
+
+pub const PROPAGULE_STAGE: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "propagule_stage",
+        0,
+        4,
+        0
+    )
+);
+
+pub const RAIL_DATA_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "rail_data_bit",
+        false
+    )
+);
+
+pub const RAIL_DIRECTION_10: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "rail_direction_10",
+        0,
+        9,
+        0
+    )
+);
+
+pub const RAIL_DIRECTION_6: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "rail_direction_6",
+        0,
+        5,
+        0
+    )
+);
+
+pub const REDSTONE_SIGNAL: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "redstone_signal",
+        0,
+        15,
+        0
+    )
+);
+
+pub const REPEATER_DELAY: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "repeater_delay",
+        0,
+        3,
+        0
+    )
+);
+
+pub const RESPAWN_ANCHOR_CHARGE: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "respawn_anchor_charge",
+        0,
+        4,
+        0
+    )
+);
+
+pub const ROTATION: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "rotation",
+        0,
+        3,
+        0
+    )
+);
+
+pub const SAND_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "sand_type",
+        SandType::VARIANTS,
+        SandType::VARIANTS[0]
+    )
+);
+
+pub const SCULK_SENSOR_PHASE: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "sculk_sensor_phase",
+        0,
+        2,
+        0
+    )
+);
+
+pub const SEA_GRASS_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "sea_grass_type",
+        SeaGrassType::VARIANTS,
+        SeaGrassType::VARIANTS[0]
+    )
+);
+
+pub const SPONGE_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "sponge_type",
+        SpongeType::VARIANTS,
+        SpongeType::VARIANTS[0]
+    )
+);
+
+pub const STABILITY: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "stability",
+        0,
+        7,
+        0
+    )
+);
+
+pub const STABILITY_CHECK: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "stability_check",
+        false
+    )
+);
+
+pub const STONE_BRICK_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "stone_brick_type",
+        StoneBrickType::VARIANTS,
+        StoneBrickType::VARIANTS[0]
+    )
+);
+
+pub const STONE_SLAB_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "stone_slab_type",
+        StoneSlabType::VARIANTS,
+        StoneSlabType::VARIANTS[0]
+    )
+);
+
+pub const STONE_SLAB_TYPE_2: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "stone_slab_type_2",
+        StoneSlabType2::VARIANTS,
+        StoneSlabType2::VARIANTS[0]
+    )
+);
+
+pub const STONE_SLAB_TYPE_3: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "stone_slab_type_3",
+        StoneSlabType3::VARIANTS,
+        StoneSlabType3::VARIANTS[0]
+    )
+);
+
+pub const STONE_SLAB_TYPE_4: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "stone_slab_type_4",
+        StoneSlabType4::VARIANTS,
+        StoneSlabType4::VARIANTS[0]
+    )
+);
+
+pub const STRIPPED_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "stripped_bit",
+        false
+    )
+);
+
+pub const STRUCTURE_BLOCK_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "structure_block_type",
+        StructureBlockType::VARIANTS,
+        StructureBlockType::VARIANTS[0]
+    )
+);
+
+pub const STRUCTURE_VOID_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "structure_void_type",
+        StructureVoidType::VARIANTS,
+        StructureVoidType::VARIANTS[0]
+    )
+);
+
+pub const SUSPENDED_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "suspended_bit",
+        false
+    )
+);
+
+pub const TALL_GRASS_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "tall_grass_type",
+        TallGrassType::VARIANTS,
+        TallGrassType::VARIANTS[0]
+    )
+);
+
+pub const TOGGLE_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "toggle_bit",
+        false
+    )
+);
+
+pub const TORCH_FACING_DIRECTION: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "torch_facing_direction",
+        TorchFacingDirection::VARIANTS,
+        TorchFacingDirection::VARIANTS[0]
+    )
+);
+
+pub const TRIGGERED_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "triggered_bit",
+        false
+    )
+);
+
+pub const TURTLE_EGG_COUNT: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "turtle_egg_count",
+        TurtleEggCount::VARIANTS,
+        TurtleEggCount::VARIANTS[0]
+    )
+);
+
+pub const TWISTING_VINES_AGE: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "twisting_vines_age",
+        0,
+        25,
+        0
+    )
+);
+
+pub const UPDATE_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "update_bit",
+        false
+    )
+);
+
+pub const UPPER_BLOCK_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "upper_block_bit",
+        false
+    )
+);
+
+pub const UPSIDE_DOWN_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "upside_down_bit",
+        false
+    )
+);
+
+pub const VINE_DIRECTION_BITS: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "vine_direction_bits",
+        0,
+        15,
+        0
+    )
+);
+
+pub const WALL_BLOCK_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "wall_block_type",
+        WallBlockType::VARIANTS,
+        WallBlockType::VARIANTS[0]
+    )
+);
+
+pub const WALL_CONNECTION_TYPE_EAST: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "wall_connection_type_east",
+        WallConnectionType::VARIANTS,
+        WallConnectionType::VARIANTS[0]
+    )
+);
+
+pub const WALL_CONNECTION_TYPE_NORTH: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "wall_connection_type_north",
+        WallConnectionType::VARIANTS,
+        WallConnectionType::VARIANTS[0]
+    )
+);
+
+pub const WALL_CONNECTION_TYPE_SOUTH: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "wall_connection_type_south",
+        WallConnectionType::VARIANTS,
+        WallConnectionType::VARIANTS[0]
+    )
+);
+
+pub const WALL_CONNECTION_TYPE_WEST: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "wall_connection_type_west",
+        WallConnectionType::VARIANTS,
+        WallConnectionType::VARIANTS[0]
+    )
+);
+
+pub const PALE_MOSS_CARPET_SIDE_EAST: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "pale_moss_carpet_side_east",
+        PaleMossCarpetSide::VARIANTS,
+        PaleMossCarpetSide::VARIANTS[0]
+    )
+);
+
+pub const PALE_MOSS_CARPET_SIDE_NORTH: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "pale_moss_carpet_side_north",
+        PaleMossCarpetSide::VARIANTS,
+        PaleMossCarpetSide::VARIANTS[0]
+    )
+);
+
+pub const PALE_MOSS_CARPET_SIDE_SOUTH: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "pale_moss_carpet_side_south",
+        PaleMossCarpetSide::VARIANTS,
+        PaleMossCarpetSide::VARIANTS[0]
+    )
+);
+
+pub const PALE_MOSS_CARPET_SIDE_WEST: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "pale_moss_carpet_side_west",
+        PaleMossCarpetSide::VARIANTS,
+        PaleMossCarpetSide::VARIANTS[0]
+    )
+);
+
+pub const TIP: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "tip",
+        false
+    )
+);
+
+pub const NATURAL: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "natural",
+        false
+    )
+);
+
+pub const WALL_POST_BIT: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "wall_post_bit",
+        false
+    )
+);
+
+pub const WEEPING_VINES_AGE: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "weeping_vines_age",
+        0,
+        25,
+        0,
+    )
+);
+
+pub const WEIRDO_DIRECTION: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "weirdo_direction",
+        0,
+        3,
+        0
+    )
+);
+
+pub const WOOD_TYPE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "wood_type",
+        WoodType::VARIANTS,
+        WoodType::VARIANTS[0]
+    )
+);
+
+pub const TRIAL_SPAWNER_STATE: Lazy<IntPropertyType> = Lazy::new(||
+    IntPropertyType::str_new(
+        "trial_spawner_state",
+        0,
+        5,
+        0
+    )
+);
+
+pub const VAULT_STATE: Lazy<EnumPropertyType> = Lazy::new(||
+    EnumPropertyType::str_new(
+        "vault_state",
+        VaultState::VARIANTS,
+        VaultState::VARIANTS[0]
+    )
+);
+
+pub const OMINOUS: Lazy<BooleanPropertyType> = Lazy::new(||
+    BooleanPropertyType::str_new(
+        "ominous",
+        false
     )
 );
