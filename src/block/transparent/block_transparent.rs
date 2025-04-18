cca16b319fd9ccd37acf4e7855ff097117ced2a8
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-04-18 20:13:44 +1200

    Revert "as_transparent_block --> as_block_transparent"
    This reverts commit 3f9181cf9c49e64c4e98ee182b8716a2e30c5c17.

--- src/block/transparent/block_transparent.rs
+++ src/block/transparent/block_transparent.rs
@@ -3,7 +3,7 @@ use crate::block::block::{AsTBlockTransparent, TBlock};
 pub trait TBlockTransparent: TBlock {}
 
 impl<T: TBlockTransparent> AsTBlockTransparent for T {
-    fn as_block_transparent(&self) -> Option<&dyn TBlockTransparent> {
+    fn as_transparent_block(&self) -> Option<&dyn TBlockTransparent> {
         Some(self)
     }
 }
