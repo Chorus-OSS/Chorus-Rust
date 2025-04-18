39e03febc3aa9a32a0218f761b91a90fe9909f9f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-04-18 17:04:19 +1200

    block_transparent

--- src/block/transparent/block_transparent.rs
+++ src/block/transparent/block_transparent.rs
@@ -1,3 +1,36 @@
+use vek::Vec3;
 use crate::block::block::Block;
+use crate::level::block::block_properties::BlockProperties;
+use crate::level::block::block_state::BlockState;
 
-trait BlockTransparent : Block {}
+trait BlockTransparent : Block {
+    fn get_state(&self) -> &BlockState;
+    fn get_position(&self) -> &Vec3<i32>;
+    fn get_level(&self) -> &todo!("Level");
+
+
+    fn get_properties() -> BlockProperties;
+    fn get_default_state() -> BlockState;
+}
+
+impl<T> Block for T where T : BlockTransparent {
+    fn get_state(&self) -> &BlockState {
+        BlockTransparent::get_state(self)
+    }
+
+    fn get_position(&self) -> &Vec3<i32> {
+        BlockTransparent::get_position(self)
+    }
+
+    fn get_level(&self) -> &todo!("Level") {
+        BlockTransparent::get_level(self)
+    }
+
+    fn get_properties() -> BlockProperties {
+        BlockTransparent::get_properties()
+    }
+
+    fn get_default_state() -> BlockState {
+        BlockTransparent::get_default_state()
+    }
+}
