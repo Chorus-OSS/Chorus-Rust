82d6fd22ad846e5c6b1029ef7ce857c1c8f9a34f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-04-19 18:37:14 +1200

    entity

--- src/entity/entity.rs
+++ src/entity/entity.rs
@@ -1,16 +1,8 @@
 use vek::Vec3;
 use crate::level::level::Level;
 
-pub trait TEntity {
-    fn get_position(&self) -> &Vec3<f32>;
-    fn get_position_mut(&mut self) -> &mut Vec3<f32>;
-    fn set_position(&mut self, pos: Vec3<f32>);
-    
-    fn get_velocity(&self) -> &Vec3<f32>;
-    fn get_velocity_mut(&mut self) -> &mut Vec3<f32>;
-    fn set_velocity(&mut self, vel: Vec3<f32>);
-    
-    fn get_level(&self) -> &Level;
-    fn get_level_mut(&mut self) -> &mut Level;
-    fn set_level(&mut self, level: Level);
+pub struct Entity {
+    position: Vec3<f32>,
+    velocity: Vec3<f32>,
+    level: Level,
 }
