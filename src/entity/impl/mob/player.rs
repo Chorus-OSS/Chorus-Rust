fec67272b33d69e927637b436839c0c2a3ab268c
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-04-20 18:33:12 +1200

    player

--- src/entity/impl/mob/player.rs
+++ src/entity/impl/mob/player.rs
@@ -1,6 +1,21 @@
+use std::sync::Arc;
 use bevy_ecs::prelude::Component;
+use crate::entity::entity::Entity;
+use crate::entity::{entity_id, entity_mob};
+use crate::entity::entity_mob::EntityMob;
+use crate::level::level::Level;
 
 #[derive(Component)]
 pub struct Player {
     
+}
+
+impl Player {
+    pub fn new(level: Arc<Level>) -> (Entity, EntityMob, Player) {
+        let entity = Entity::default(entity_id::PLAYER.to_string(), rand::random::<i64>(), level);
+        let entity_mob = EntityMob::default();
+        let player = Player {};
+        
+        (entity, entity_mob, player)
+    }
 }
